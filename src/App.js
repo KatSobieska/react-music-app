f9026c8f142b5f29abbee76d9dffb4149d182f47
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-07-20 10:31:03 +0200

    Add primary structure of first page and import it to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Menu from "./components/views/Menu";
 import Container from "./components/common/Container";
+import FirstPage from "./components/views/FirstPage";
 
 function App() {
   return (
     <Container>
       <Menu />
+      <FirstPage />
     </Container>
   );
 }
---
+++ src/components/views/FirstPage.js
@@ -0,0 +1,12 @@
+import { Container, Row } from "react-bootstrap";
+
+const FirstPage = () => {
+  return (
+    <Container>
+      <Row>TOPBANNER</Row>
+      <Row>HOME</Row>
+    </Container>
+  );
+};
+
+export default FirstPage;
