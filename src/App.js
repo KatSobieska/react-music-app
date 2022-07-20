8d83b37efdbbc41edc9ff6fd79f903e1a9791b7d
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-07-20 10:09:25 +0200

    Import Container to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import Menu from "./components/views/Menu";
+import Container from "./components/common/Container";
 
 function App() {
-  return <Menu />;
+  return (
+    <Container>
+      <Menu />
+    </Container>
+  );
 }
 
 export default App;
