efa1e51198630407daaa50b3bd2bc1b8bf5cee79
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-07-21 20:20:44 +0200

    Import CategoryBox and render based on state

--- src/components/features/Home.js
+++ src/components/features/Home.js
@@ -1,7 +1,40 @@
+import { getAllCategories } from "../../redux/categoriesRedux";
+import CategoryBox from "../common/CategoryBox";
+import AudioPlayer from "./AudioPlayer";
 import styles from "./Home.module.scss";
+import { useSelector } from "react-redux";
+import PropTypes from "prop-types";
 
 const Home = () => {
-  return <div className={styles.home}>HOME</div>;
+  const categories = useSelector(getAllCategories);
+
+  console.log("categories", categories);
+  return (
+    <div className={styles.home}>
+      <div className={styles.first}>
+        <div className={styles.popular}>CZESC</div>
+        <div className={styles.categories}>
+          <p>Categories</p>
+          <div className={styles.categoryBoxes}>
+            {categories.map((category, index) => (
+              <CategoryBox
+                key={index}
+                name={category.name}
+                imageName={category.imageName}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className={styles.discover}>WITAJ</div>
+    </div>
+  );
+};
+
+Home.propTypes = {
+  id: PropTypes.string,
+  name: PropTypes.string,
+  imageName: PropTypes.string,
 };
 
 export default Home;
