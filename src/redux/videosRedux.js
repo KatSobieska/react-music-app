fbe73eb89b08b4d3042899f8634136ad943bb061
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-07-21 18:17:55 +0200

    Add videos to the state and create videosRedux

---
+++ src/redux/videosRedux.js
@@ -0,0 +1,8 @@
+export const getAllVideos = (state) => state.videos;
+
+export default function reducer(statePart = [], action = []) {
+  switch (action.type) {
+    default:
+      return statePart;
+  }
+}
