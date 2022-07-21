c4794b4a0ebf74613b903af65ca4f48db79a6976
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-07-21 13:15:50 +0200

    Import component Search

--- src/components/views/BannerTop.js
+++ src/components/views/BannerTop.js
@@ -1,9 +1,11 @@
+import Search from "../features/Search";
 import styles from "./BannerTop.module.scss";
 
 const BannerTop = () => {
   return (
     <div className={styles.banner}>
       <img className={styles.photo} src={`/images/bannerTop.png`} alt=""></img>
+      <Search />
     </div>
   );
 };
