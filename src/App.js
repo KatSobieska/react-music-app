49c81c33899d47d6804c08431f64643a173126ad
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-07-21 15:00:39 +0200

    Add Routes and import component for pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import Menu from "./components/views/Menu";
 import Container from "./components/common/Container";
+import ContainerPage from "./components/common/ContainerPage";
 import FirstPage from "./components/pages/FirstPage";
+import { Route, Routes } from "react-router";
+import Home from "./components/features/Home";
+import Categories from "./components/pages/Categories";
+import Favourite from "./components/pages/Favourite";
 
 function App() {
   return (
     <Container>
       <Menu />
-      <FirstPage />
+      <ContainerPage>
+        <FirstPage />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/favourite" element={<Favourite />} />
+        </Routes>
+      </ContainerPage>
     </Container>
   );
 }
