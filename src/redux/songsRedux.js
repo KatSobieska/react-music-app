a28bf86e6bf79a22351df34751cdc00e7e239544
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-11-06 19:27:43 +0100

    Add selector for recommended songs

--- src/redux/songsRedux.js
+++ src/redux/songsRedux.js
@@ -5,6 +5,9 @@ export const getSongById = ({ songs }, songId) =>
 export const getSongByCategory = ({ songs }, category) =>
   songs.filter((song) => song.category === category);
 
+export const getRecommendedSongs = ({ songs }) =>
+  songs.filter((song) => song.recommended === true);
+
 export default function reducer(statePart = [], action = []) {
   switch (action.type) {
     default:
