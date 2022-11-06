c1476be84aede07ed630a92f03895af5b17fede9
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-11-06 19:29:10 +0100

    Update audio player component and add responsive styles

--- src/components/features/AudioPlayer.js
+++ src/components/features/AudioPlayer.js
@@ -1,5 +1,27 @@
-const AudioPlayer = () => {
-  return <div></div>;
+import ReactAudioPlayer from "react-audio-player";
+import styles from "./AudioPlayer.module.scss";
+import React from "react";
+import PropTypes from "prop-types";
+
+const AudioPlayer = (props) => {
+  AudioPlayer.propTypes = {
+    id: PropTypes.string,
+    fileName: PropTypes.string,
+    songImage: PropTypes.string,
+  };
+
+  return (
+    <div className={styles.player}>
+      <img
+        src={`/images/${props.songImage}.png`}
+        alt=""
+        width={55}
+        height={55}
+        style={{ borderRadius: "15px" }}
+      ></img>
+      <ReactAudioPlayer src={`/songs/${props.fileName}`} controls />
+    </div>
+  );
 };
 
 export default AudioPlayer;
