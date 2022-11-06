e77d5755c9dc75d4c80f8fafd9534b404d76dbb6
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-11-06 19:30:09 +0100

    Update component Favourite and the styles

--- src/components/pages/Favourite.js
+++ src/components/pages/Favourite.js
@@ -1,7 +1,52 @@
+import AudioPlayer from "../features/AudioPlayer";
 import styles from "./Favourite.module.scss";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getAllFavorites,
+  removeFromFavorites,
+} from "../../redux/favoritesRedux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 const Favourite = () => {
-  return <div className={styles.body}> FAVOURITE</div>;
+  const favoriteSongs = useSelector(getAllFavorites);
+  const dispatch = useDispatch();
+
+  const handleUnfavorite = (id) => {
+    dispatch(removeFromFavorites(id));
+  };
+
+  return (
+    <>
+      {favoriteSongs.length !== 0 ? (
+        <div className={styles.body}>
+          {favoriteSongs.map((song, index) => (
+            <div className={styles.favSong} key={index}>
+              <AudioPlayer
+                fileName={song.fileName}
+                songImage={song.songImage}
+                styles={{ borderRadius: "15px" }}
+              />
+              <button
+                onClick={() => handleUnfavorite(song.id)}
+                className={styles.buttonFav}
+              >
+                <FontAwesomeIcon
+                  icon={faTrash}
+                  style={{ marginBottom: "14px" }}
+                  size="2x"
+                />
+              </button>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div className={styles.noFavorites}>
+          <p>You didn't add any song to favorites :(</p>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Favourite;
