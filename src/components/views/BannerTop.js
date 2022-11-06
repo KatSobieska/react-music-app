d7ed75d8e0a821cb960620860ead7ebe8a894c45
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-11-06 17:49:12 +0100

    Change image and update the styles

--- src/components/views/BannerTop.js
+++ src/components/views/BannerTop.js
@@ -4,7 +4,7 @@ import styles from "./BannerTop.module.scss";
 const BannerTop = () => {
   return (
     <div className={styles.banner}>
-      <img className={styles.photo} src={`/images/bannerTop.png`} alt=""></img>
+      <img className={styles.photo} src={`/images/bannerTop1.png`} alt=""></img>
       <Search />
     </div>
   );
